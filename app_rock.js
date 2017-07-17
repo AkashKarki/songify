7cc67ee89e3018475980967d1ecba4b4e592918f
akash <akashkarki10@gmail.com>
2017-07-17 21:50:47 +0530

    all objective done

--- app_rock.js
+++ app_rock.js
@@ -1,30 +1,30 @@
     var index = 0;
     carousel();
 
-    function carousel() {
+    function carousel() {       //for showing slide show
         var i;
         var x = document.getElementsByClassName("slide");
         for (i = 0; i < x.length; i++) {
-       x[i].style.display = "none";  
+       x[i].style.display = "none";  //for making display of each image to none
         }
     index++;
     if (index > x.length) {
         index = 1;
     }    
-    x[index-1].style.display = "block";  
+    x[index-1].style.display = "block";  //showing selected image
     setTimeout(carousel, 9000);    
 }
     var song = document.querySelector('audio');
-    window.onload = setInterval(function(){ var song = document.querySelector('audio');
+    window.onload = setInterval(function(){ var song = document.querySelector('audio');//setting time in footer
         document.getElementById('duration').value=fancyTimeFormat(song.duration);
         document.getElementById('current_time').value=fancyTimeFormat(song.currentTime);
      }, 1000)
-    song.addEventListener('timeupdate', onLoadProgress);
+    song.addEventListener('timeupdate', onLoadProgress); //for increasing progress bar
     var myProgressBar = document.getElementById('seekbar'); 
     function onLoadProgress () {
     var progress= parseInt(((song.currentTime / song.duration) * 100), 10);
     console.log(progress);
-    myProgressBar.value = progress; 
+    myProgressBar.value = progress;  //setting value in progress bar
 }
     function fancyTimeFormat(time)
 {   
@@ -55,7 +55,7 @@
     document.getElementById("cover2").src=song_image[0];
     document.getElementById('foot_name').value=SongName[0];
     document.getElementById('foot_album').value=SongAlbum[0];
-    for(i=0;i<3;i++)
+    for(i=0;i<3;i++) //setting value in table first
     {
         var name='#song'+(i+1);
          $(name).find('.song_image').attr("src", song_image[i]);
@@ -64,43 +64,43 @@
         $(name).find('.song_album').text(SongAlbum[i]);
         $(name).find('.song_duration').text(SongDuration[i])
     }
-    $('#songs').DataTable({
+    $('#songs').DataTable({ //using table for search
         });
 
-    $('.icon').on('click',addSongNameClickEvent);
+    $('.icon').on('click',addSongNameClickEvent); //function for playing and pausing song and changing play pause icon
     function addSongNameClickEvent()
     {
         var song=$('audio');
         if (document.getElementById("player").paused==true){
                 song[0].play();
-                $('.icon').removeClass('fa fa-play-circle');
-                $(".icon").addClass("fa fa-pause-circle");
+                $('.icon').removeClass('fa fa-play-circle');//chaining
+                $(".icon").addClass("fa fa-pause-circle");//chaining
                 //$(".fa-pause-circle").addClass("iconshow");
                 console.log("changed play");
             }
             else{
                 song[0].pause();
                 //$(".fa-play-circle").addClass("iconshow");
-                $('.icon').removeClass('fa fa-pause-circle')
-                $('.icon').addClass('fa fa-play-circle');
+                $('.icon').removeClass('fa fa-pause-circle');//chaining
+                $('.icon').addClass('fa fa-play-circle');//chaining
                 console.log("changed pause");
             }
     }
-     $('body').on('keypress', function(event) {
+     $('body').on('keypress', function(event) { //function for playing and pausing by keypress
                 if (event.keyCode == 32 && event.target.tagName != "INPUT") {
                     addSongNameClickEvent();
                 }
             });
-    function songPlay(id){
+    function songPlay(id){// palying song
         var song=$('audio');
         var current_song = $('audio').attr('src');
         var request_song=SongName[id]+'.mp3';
         if(current_song!=request_song){
             $(song).attr('src',request_song)
             song[0].load();
-            document.getElementById("cover2").src=song_image[id];
-            document.getElementById('foot_name').value=SongName[id];
-            document.getElementById('foot_album').value=SongAlbum[id];
+            document.getElementById("cover2").src=song_image[id]; //change song image dynamically on footer
+            document.getElementById('foot_name').value=SongName[id];//change song name dynamically on footer
+            document.getElementById('foot_album').value=SongAlbum[id];//change song Artist dynamically on footer
             song[0].play();
             $(".icon").addClass("fa fa-pause-circle");
             //$(".fa-pause-circle").addClass("iconshow");
